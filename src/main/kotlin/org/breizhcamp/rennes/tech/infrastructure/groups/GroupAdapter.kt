19fd87236d1ae02ed3258303e65c06e5a0ad692c
Alexandre Thomazo <alex@thomazo.info>
2026-04-05 21:27:09 +0200

    [pom] Migration vers Spring Boot 4.0.5

--- src/main/kotlin/org/breizhcamp/rennes/tech/infrastructure/groups/GroupAdapter.kt
+++ src/main/kotlin/org/breizhcamp/rennes/tech/infrastructure/groups/GroupAdapter.kt
@@ -1,19 +1,19 @@
 package org.breizhcamp.rennes.tech.infrastructure.groups
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.breizhcamp.rennes.tech.config.annotations.Adapter
 import org.breizhcamp.rennes.tech.domain.entities.Group
 import org.breizhcamp.rennes.tech.domain.ports.GroupPort
 import org.breizhcamp.rennes.tech.infrastructure.groups.dto.GroupListConfig
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.core.io.ClassPathResource
+import tools.jackson.dataformat.yaml.YAMLMapper
+import tools.jackson.module.kotlin.kotlinModule
+import tools.jackson.module.kotlin.readValue
 
 @Adapter
 class GroupAdapter: GroupPort {
-    private val yamlMapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
+    private val yamlMapper = YAMLMapper.builder()
+        .addModule(kotlinModule())
+        .build()
 
     override fun list(): List<Group> = yamlMapper
         .readValue<GroupListConfig>(ClassPathResource("groups.yaml").inputStream).groups
